211674c3d3cda1f7a089ac76d7d57a349db21d42
Raman Lysav <3953931@gmail.com>
2025-07-29 22:27:23 +0300

    v0.7.14

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .data import user_data
-from .brand import choose_brand
-from .model import text_handler
-from .flow import year_handler, nav_handler, phone_handler
+from handlers.brand import choose_brand
+from handlers.model import text_handler
+from handlers.flow import year_handler, nav_handler, phone_handler
